c64113c8c264cf0d3f2709b4618799059daeb58a
Chirag Juneja <chiragjuneja6@gmail.com>
2025-07-03 17:10:54 +0530

    langgraph chatbot with memory

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,24 +1,27 @@
 import io
-import json
 from PIL import Image
 from typing import Annotated
 from typing_extensions import TypedDict
 
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import StateGraph, START
 from langgraph.graph.message import add_messages
 
 from langchain.chat_models import init_chat_model
 from langchain_community.tools import DuckDuckGoSearchResults
-from langchain_core.messages import ToolMessage
 from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
 
 # Globals
-MODEL = "llama3.2:3b"
+MODEL = "qwen3:4b"
 PROVIDER = "ollama"
+
+# Memory
+# Use SqliteSaver or PostgresSaver in prod
+memory = MemorySaver()
 
 # Tools
 search = DuckDuckGoSearchResults(
-    region="in-en", max_output=3, output_format="json")
+    region="in-en", max_results=3, output_format="json")
 
 tools = [search]
 
@@ -48,43 +51,7 @@ def chatbot(state: State):
     return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
-class BasicToolNode:
-    def __init__(self, tools):
-        self.tools_by_name = {tool.name: tool for tool in tools}
-
-    def __call__(self, inputs):
-        if messages := inputs.get("messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("No message found in input")
-        outputs = []
-        for tool_call in message.tool_calls:
-            tool_result = self.tools_by_name[tool_call["name"]].invoke(
-                tool_call["args"]
-            )
-            outputs.append(
-                ToolMessage(
-                    content=json.dumps(tool_result),
-                    name=tool_call["name"],
-                    tool_call_id=tool_call["id"]
-                )
-            )
-        return {"messages": outputs}
-
-
 tool_node = ToolNode(tools=tools)
-
-# Routing
-
-
-def route_tools(state: State):
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
-    return END
 
 
 # Graph
@@ -96,19 +63,24 @@ graph_builder.add_conditional_edges(
     "chatbot", tools_condition)
 graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
-graph = graph_builder.compile()
+graph = graph_builder.compile(checkpointer=memory)
 
 # show_graph(graph)
 
+config = {"configurable": {"thread_id": "1"}}
+
 
 def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
-        for value in event.values():
-            # print("Assistant:", value["messages"][-1].content)
-            value["messages"][-1].pretty_print()
+    events = graph.stream(
+        {"messages": [{"role": "user", "content": user_input}]},
+        config,
+        stream_mode="values"
+    )
+    for event in events:
+        event["messages"][-1].pretty_print()
 
 
-# user_input = "Who is the PM of India?"
+# user_input = "Hi, My name is Bob"
 # stream_graph_updates(user_input)
 
 if __name__ == "__main__":
